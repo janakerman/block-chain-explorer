02dfbda2194acdfe3e29a7340befb1ae84bfe104
Jan Akerman <jakerman@scottlogic.co.uk>
2015-03-12 13:21:04 +0000

    Pulls master page file inside of closure.

--- app/masterPage/masterPage.js
+++ app/masterPage/masterPage.js
@@ -1,21 +1,24 @@
 'use strict';
 
-var masterPage = angular.module('myApp.masterPage', ['ngRoute', 'blockExplorerServices']);
+(function() {
+  var masterPage = angular.module('myApp.masterPage', ['ngRoute', 'blockExplorerServices']);
 
-masterPage.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'masterPage/masterPage.html',
-    controller: 'MasterPageCtrl'
-  });
-}]);
-
-masterPage.controller('MasterPageCtrl', ['$http', 'BlockService', function($http, BlockService) {
-  var self = this;
-
-  BlockService.getLatestHash().then(function(latestHash) {
-    BlockService.getBlocks(latestHash, 10).then(function(blocksArray) {
-      self.rawBlocks = blocksArray;
+  masterPage.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'masterPage/masterPage.html',
+      controller: 'MasterPageCtrl'
     });
-  });
+  }]);
 
-}]);
+  masterPage.controller('MasterPageCtrl', ['$http', 'BlockService', function($http, BlockService) {
+    var self = this;
+
+    BlockService.getLatestHash().then(function(latestHash) {
+      BlockService.getBlocks(latestHash, 10).then(function(blocksArray) {
+        self.rawBlocks = blocksArray;
+      });
+    });
+
+  }]);
+
+})();
