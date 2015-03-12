539fb30d221207e706a3495e8fdc2a888c8fecef
Jan Akerman <jakerman@scottlogic.co.uk>
2015-03-12 09:25:20 +0000

    Master controller now requests 10 latest blocks.

--- app/masterPage/masterPage.js
+++ app/masterPage/masterPage.js
@@ -1,14 +1,43 @@
 'use strict';
 
-angular.module('myApp.masterPage', ['ngRoute'])
+var masterPage = angular.module('myApp.masterPage', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+masterPage.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'masterPage/masterPage.html',
     controller: 'MasterPageCtrl'
   });
-}])
+}]);
 
-.controller('MasterPageCtrl', [function() {
+masterPage.controller('MasterPageCtrl', ['$http', function($http) {
+
+  this.rawBlocks = [];
+
+  $http.get('/blockExplorer/q/latesthash').then(function(response) {
+    this.getBlocks(10, response.data);
+  }.bind(this));
+
+  this.getBlocks = function(maxBlocks, fromHash) {
+    this.chainGetBlock(this.getBlock(fromHash, maxBlocks));
+  }
+
+  this.chainGetBlock = function(previousGet, maxBlocks) {
+
+    if (this.rawBlocks.length === maxBlocks) {
+      return;
+    }
+
+    previousGet.then(this.onBlockResponse.bind(this));
+  };
+
+  this.onBlockResponse = function(response) {
+    var rawBlock = response.data;
+    this.rawBlocks.push(rawBlock);
+    this.chainGetBlock(this.getBlock(rawBlock['prev_block']));
+  }
+
+  this.getBlock = function(hash) {
+    return $http.get('/blockExplorer/rawblock/' + hash);
+  }
 
 }]);
