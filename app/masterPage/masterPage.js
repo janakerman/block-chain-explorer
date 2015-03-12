1f52519dc35cf92212766cab75d3739630a18854
Jan Akerman <jakerman@scottlogic.co.uk>
2015-03-12 17:35:20 +0000

    Previous ten button now loads previous ten blocks.

--- app/masterPage/masterPage.js
+++ app/masterPage/masterPage.js
@@ -13,11 +13,27 @@
   masterPage.controller('MasterPageCtrl', ['$http', 'BlockService', function($http, BlockService) {
     var self = this;
 
+    self.oldestBlock = '';
+
     BlockService.getLatestHash().then(function(latestHash) {
-      BlockService.getBlocks(latestHash, 10).then(function(blocksArray) {
-        self.rawBlocks = blocksArray;
-      });
+      self.previousTenBlocks(latestHash);
     });
+
+    this.previousTenBlocks = function (hash) {
+      BlockService.getBlocks(hash, 10).then(function(blocksArray) {
+        self.rawBlocks = blocksArray;
+
+        self.oldestBlock = self.rawBlocks[self.rawBlocks.length-1].hash;
+      });
+    }
+
+    this.previousTen = function () {
+      self.previousTenBlocks(self.oldestBlock);
+    }
+
+    this.convertHexToDecimal = function(hex) {
+      return parseInt(hex, 16);
+    }
 
   }]);
 
