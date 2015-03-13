34ad079dce9eb1d6d7b2ac421dbda5b4d978a5b3
Jan Akerman <jakerman@scottlogic.co.uk>
2015-03-13 12:30:41 +0000

    Adds transaction browser directive.

--- app/blockDetail/blockDetail.js
+++ app/blockDetail/blockDetail.js
@@ -18,4 +18,69 @@
       });
     });
   }]);
+
+  blockDetailPage.directive('transactionBrowser', function() {
+    return {
+      templateUrl: 'blockDetail/transactionBrowser.html',
+      restrict: 'E',
+      scope: {
+        transactions: '=transactions',
+      },
+      controller: function($scope) {
+        var vm = this;
+
+        $scope.$watch(function() {
+            return $scope.transactions;
+          }, function() {
+
+            if (!$scope.transactions) {
+              return;
+            }
+
+            vm.transactions = $scope.transactions;
+
+            if (vm.transactions.length == 0) {
+             return;
+            }
+
+            vm.currentIndex = 0;
+            vm.currentTransactionsFromIndex(vm.currentIndex, 10);
+          });
+
+          vm.currentTransactionsFromIndex = function (index, number) {
+            vm.currentTransactions = vm.transactions.slice(index, index+number);
+          }
+
+          vm.previousTen = function () {
+            if (vm.currentIndex < 10) {
+              return;
+            }
+
+            vm.currentIndex -= 10;
+
+            vm.currentTransactionsFromIndex(vm.currentIndex, 10);
+          }
+
+          vm.nextTen = function () {
+            if (vm.currentIndex > vm.transactions.length-10) {
+              return;
+            }
+
+            vm.currentIndex += 10;
+
+            vm.currentTransactionsFromIndex(vm.currentIndex, 10);
+          }
+
+          vm.remainingTransactions = function() {
+            if (!vm.transactions) {
+              return;
+            }
+
+            return Math.min(vm.transactions.length, vm.currentIndex + 10);
+          }
+      },
+      controllerAs: 'controller',
+    }
+  });
+
 })();
