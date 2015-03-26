bfb9ad029b2294e743ce1eda9defee289ba0b483
Jan Akerman <jakerman@scottlogic.co.uk>
2015-03-26 09:07:19 +0000

    Visualisation is now appended in correct place.

--- app/blockDetail/blockDetail.js
+++ app/blockDetail/blockDetail.js
@@ -130,7 +130,7 @@
           nodes[0].cy = height/2;
 
 
-          var svg = d3.select('body').append('svg')
+          var svg = d3.select('svg')
               .attr('width', width)
               .attr('height', height);
 
